2da5fc60c1454d1f66aba45a2cc7fedde5f7346a
srinivasankalap <cnu.ankalap@gmail.com>
2023-09-12 16:12:20 +0530

    MealsSummary

---
+++ src/components/Meals/Meals.js
@@ -0,0 +1,13 @@
+import MealsSummary from "./MealsSummary";
+import AvailabeMeals from "./AvailableMeals";
+
+const Meals=()=>{
+    return (
+        <>
+            <MealsSummary/>
+            <AvailabeMeals/>
+        </>
+    )
+}
+
+export default Meals;
---
+++ src/components/Meals/MealsSummary.js
@@ -0,0 +1,17 @@
+import classes from './MealsSummary.module.css';
+
+const MealsSummary=()=>{
+    return(
+        <section className={classes.summary}>
+            <h2>Delivering Deliciousness to Your Doorstep</h2>
+            <p>
+            With a vast selection of restaurants and cuisines at your fingertips, our food delivery app ensures that you can savor gourmet meals, comfort food, and international flavors without leaving the comfort of your own space.
+            </p>
+            <p>
+            Whether it's a quick bite or a sumptuous feast, we're here to make dining at home an unforgettable experience.
+            </p>
+        </section>
+    )
+}
+
+export default MealsSummary;
